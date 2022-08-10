843d2172e85a8cca5ce7fdcb3cf7e22e901ca46f
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2022-08-10 18:57:25 +0100

    Update `gpkg.py` docstrings

--- src/pyspark_vector_files/gpkg.py
+++ src/pyspark_vector_files/gpkg.py
@@ -1,44 +1,160 @@
-"""Functions for reading a GPKG using Spark's JDBC drivers.
+"""Functions for reading a GeoPackage using Spark's JDBC drivers.
+
+Reading a GeoPackage using Spark's JDBC drivers.
+===================================================
 
 Register the GeoPackage Dialect
-===============================
+-------------------------------
 
-To register the GeoPackage dialect, run the following in a Databricks notebook cell:
+To read a GeoPackage using Spark's JDBC drivers, you will need to register a custom
+mapping of GeoPackage to Spark Catalyst types.
 
-```scala
-%scala
-import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects}
-import org.apache.spark.sql.types._
 
-object GeoPackageDialect extends JdbcDialect {
-  override def canHandle(url: String): Boolean = url.startsWith("jdbc:sqlite")
+To do this, run the following in a Databricks notebook cell:
 
-  override def getCatalystType(
-    sqlType: Int,
-    typeName: String,
-    size: Int,
-    md: MetadataBuilder
-  ): Option[DataType] = typeName match {
-    case "BOOLEAN" => Some(BooleanType)
-    case "TINYINT" => Some(ByteType)
-    case "SMALLINT" => Some(ShortType)
-    case "MEDIUMINT" => Some(IntegerType)
-    case "INT" | "INTEGER" => Some(LongType)
-    case "FLOAT" => Some(FloatType)
-    case "DOUBLE" | "REAL" => Some(DoubleType)
-    case "TEXT" => Some(StringType)
-    case "BLOB" => Some(BinaryType)
-    case "GEOMETRY" | "POINT" | "LINESTRING" | "POLYGON" | "MULTIPOINT" |
-      "MULTILINESTRING" | "MULTIPOLYGON" | "GEOMETRYCOLLECTION" | "CIRCULARSTRING" |
-      "COMPOUNDCURVE" | "CURVEPOLYGON" | "MULTICURVE" | "MULTISURFACE" | "CURVE" |
-      "SURFACE" => Some(BinaryType)
-    case "DATE" => Some(DateType)
-    case "DATETIME" => Some(StringType)
-  }
-}
+.. code-block:: scala
 
-JdbcDialects.registerDialect(GeoPackageDialect)
-```
+    %scala
+    import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects}
+    import org.apache.spark.sql.types._
+
+    object GeoPackageDialect extends JdbcDialect {
+    override def canHandle(url: String): Boolean = url.startsWith("jdbc:sqlite")
+
+    override def getCatalystType(
+        sqlType: Int,
+        typeName: String,
+        size: Int,
+        md: MetadataBuilder
+    ): Option[DataType] = typeName match {
+        case "BOOLEAN" => Some(BooleanType)
+        case "TINYINT" => Some(ByteType)
+        case "SMALLINT" => Some(ShortType)
+        case "MEDIUMINT" => Some(IntegerType)
+        case "INT" | "INTEGER" => Some(LongType)
+        case "FLOAT" => Some(FloatType)
+        case "DOUBLE" | "REAL" => Some(DoubleType)
+        case "TEXT" => Some(StringType)
+        case "BLOB" => Some(BinaryType)
+        case "GEOMETRY" | "POINT" | "LINESTRING" | "POLYGON" | "MULTIPOINT" |
+        "MULTILINESTRING" | "MULTIPOLYGON" | "GEOMETRYCOLLECTION" | "CIRCULARSTRING" |
+        "COMPOUNDCURVE" | "CURVEPOLYGON" | "MULTICURVE" | "MULTISURFACE" | "CURVE" |
+        "SURFACE" => Some(BinaryType)
+        case "DATE" => Some(DateType)
+        case "DATETIME" => Some(StringType)
+    }
+    }
+
+    JdbcDialects.registerDialect(GeoPackageDialect)
+
+Once you've registered the GeoPackage dialect, you can list the layers in a given
+GeoPackage, get the coordinate reference system of a given layer, or read a layer
+into a Spark DataFrame with the geometry column encoded as Well Known Binary.
+
+List layers
+-----------
+
+To list the layers in a given GeoPackage, use :func:`list_layers`:
+
+.. code-block:: python
+
+    list_layers(
+        path="/path/to/file.gpkg"
+    )
+
+Get the coordinate reference system of a layer
+----------------------------------------------
+
+To get the coordinate reference system of a layers in a given GeoPackage, use
+:func:`get_crs`:
+
+.. code-block:: python
+
+    get_crs(
+        path="/path/to/file.gpkg"
+        layer_name="layer_name"
+    )
+
+Read a layer into a Spark DataFrame
+-----------------------------------
+
+To read a layer into a Spark DataFrame, use :func:`read_gpkg`, If you don't supply
+a layer, the first layer will be read:
+
+.. code-block:: python
+
+    read_gpkg(
+        path="/path/to/file.gpkg"
+    )
+
+Read a specific layer
+^^^^^^^^^^^^^^^^^^^^^
+
+If you supply a layer, that will be read:
+
+.. code-block:: python
+
+    read_gpkg(
+        path="/path/to/file.gpkg"
+        layer_name="layer_name"
+   )
+
+Supply a custom geometry column name
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+The function will assume that the geometry column is called `geom`, if it's called
+something else, supply a different `original_geometry_column_name`:
+
+.. code-block:: python
+
+    read_gpkg(
+        path="/path/to/file.gpkg"
+        layer_name="layer_name"
+        original_geometry_column_name="geometry",
+    )
+
+GeoPackage Geometry Encoding
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+`GeoPackage Geometry Encoding`_ consists of two parts:
+
+1. A GeoPackage Binary (GPB) header
+2. A Well Known Binary (WKB) geometry
+
+By default, the function will split the original geometry column into these two
+parts, unpack the GPB header, and return both as, respectively, `gpb_header` and
+`wkb_geometry`.
+
+If you don't want the GPB header, set `drop_gpb_header` to `True`:
+
+.. code-block:: python
+
+    read_gpkg(
+        path="/path/to/file.gpkg"
+        layer_name="layer_name"
+        drop_gpb_header=True,
+    )
+
+The length of the GPB header depends on the format of envelope (bounding box) included
+in it. The function assumes that the envelope will be [minx, maxx, miny, maxy] and
+that, therefore, the GPB header will be 40 bytes long. However, it's possible for the
+format of the envelope to be [minx, maxx, miny, maxy, minz, maxz] or [minx, maxx, miny,
+maxy, minm, maxm] and, therefore 56 bytes long, or [minx, maxx, miny, maxy, minz, maxz,
+minm, maxm] and, therefore, 72 bytes long.
+
+In these cases you will need to set `header_length` to the appropriate value:
+
+.. code-block:: python
+
+    read_gpkg(
+        path="/path/to/file.gpkg"
+        layer_name="layer_name"
+        header_length=56,
+    )
+
+.. _`GeoPackage Geometry Encoding`:
+    https://www.geopackage.org/spec/#gpb_format
+
 """
 from functools import partial
 from struct import unpack
@@ -60,14 +176,14 @@ HEADER_LENGTH = 40
 
 
 def _read_gpkg(
-    filepath: str,
+    path: str,
     layer_name: str,
     spark: SparkSession,
 ) -> SparkDataFrame:
     """Read a given table from a GeoPackage/Sqlite file."""
     return (
         spark.read.format("jdbc")
-        .option("url", f"jdbc:sqlite:{filepath}")
+        .option("url", f"jdbc:sqlite:{path}")
         .option("dbtable", layer_name)
         .load()
     )
@@ -128,7 +244,7 @@ def _split_geometry(
 
 
 def list_layers(
-    filepath: str,
+    path: str,
     spark: Optional[SparkSession] = None,
 ) -> List[str]:
     """List layers from a GeoPackage.
@@ -140,7 +256,7 @@ def list_layers(
         ['reference_parcels']
 
     Args:
-        filepath (str): Path to the GeoPackage. Must use the File API Format, i.e.
+        path (str): Path to the GeoPackage. Must use the File API Format, i.e.
             start with `/dbfs` instead of `dbfs:`.
         spark (Optional[SparkSession], optional): The SparkSession to use. If none
              is provided, the active session will be used. Defaults to None.
@@ -150,14 +266,14 @@ def list_layers(
     """  # noqa: B950
     _spark = spark if spark else SparkSession.getActiveSession()
     gpkg_contents = _get_gpkg_contents(
-        filepath=filepath,
+        path=path,
         spark=_spark,
     )
     return gpkg_contents.select(collect_list("table_name")).first()[0]
 
 
 def get_crs(
-    filepath: str,
+    path: str,
     layer_name: str,
     spark: Optional[SparkSession] = None,
 ) -> str:
@@ -171,7 +287,7 @@ def get_crs(
         'PROJCS["OSGB 1936 / British National Grid",GEOGCS["OSGB 1936",DATUM["OSGB_1936",SPHEROID["Airy 1830",6377563.396,299.3249646,AUTHORITY["EPSG","7001"]],AUTHORITY["EPSG","6277"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4277"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",49],PARAMETER["central_meridian",-2],PARAMETER["scale_factor",0.9996012717],PARAMETER["false_easting",400000],PARAMETER["false_northing",-100000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","27700"]]'
 
     Args:
-        filepath (str): Path to the GeoPackage. Must use the File API Format, i.e.
+        path (str): Path to the GeoPackage. Must use the File API Format, i.e.
             start with `/dbfs` instead of `dbfs:`.
         layer_name (str): The name of the layer to read.
         spark (Optional[SparkSession], optional): The SparkSession to use. If none
@@ -185,7 +301,7 @@ def get_crs(
     """  # noqa: B950
     _spark = spark if spark else SparkSession.getActiveSession()
     gpkg_contents = _get_gpkg_contents(
-        filepath=filepath,
+        path=path,
         spark=_spark,
     )
     srs_id = (
@@ -194,7 +310,7 @@ def get_crs(
         .first()[0]
     )
     gpkg_spatial_ref_sys = _read_gpkg(
-        filepath=filepath,
+        path=path,
         layer_name="gpkg_spatial_ref_sys",
         spark=_spark,
     )
@@ -206,7 +322,7 @@ def get_crs(
 
 
 def read_gpkg(
-    filepath: str,
+    path: str,
     original_geometry_column_name: str = "geom",
     header_length: int = HEADER_LENGTH,
     drop_gpb_header: bool = False,
@@ -216,7 +332,7 @@ def read_gpkg(
     """Read a GeoPackage into Spark DataFrame.
 
     Args:
-        filepath (str): Path to the GeoPackage. Must use the File API Format, i.e.
+        path (str): Path to the GeoPackage. Must use the File API Format, i.e.
             start with `/dbfs` instead of `dbfs:`.
         original_geometry_column_name (str): The name of the geometry column
             of the layer in the GeoPackage. Defaults to "geom".
@@ -242,13 +358,13 @@ def read_gpkg(
         layer_name
         if layer_name
         else list_layers(
-            filepath=filepath,
+            path=path,
             spark=_spark,
         )[0]
     )
 
     df = _read_gpkg(
-        filepath=filepath,
+        path=path,
         layer_name=_layer_name,
         spark=_spark,
     )
